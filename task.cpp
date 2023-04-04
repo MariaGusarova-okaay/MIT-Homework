a74c90a58ef859c2463d00470f82346e86551426
Maria Gusarova <gusarovamaria993@gmail.com>
2023-04-04 19:42:13 +0300

    Добавлено рекурсивное вычисление чисел Фибоначчи, начало последовательности - (1, 1, ...)

--- task.cpp
+++ task.cpp
@@ -1,6 +1,15 @@
 #include<iostream>
 using namespace std;
 
+int fib(int n){
+	if (n == 1 || n == 2) return 1;
+	return fib(n - 2) + fib(n - 1);
+}
+
+
 int main(){
 	cout << "Hello, World!" << endl;
+
+	int n; cout << "n = "; cin >> n;
+	cout << fib(n);
 }
